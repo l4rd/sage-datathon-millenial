955e39c8ab64202d71fa2613890cda16caea5e44
Cameron Hunter <bg69gl@student.sunderland.ac.uk>
2017-01-19 17:17:49 +0000

    Toolbar removed, action bar back

--- app/src/main/java/com/millennial/sageup/UserDetails.java
+++ app/src/main/java/com/millennial/sageup/UserDetails.java
@@ -11,9 +11,6 @@ public class UserDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
 
-        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        // setting Toolbar as Action Bar for the App
-        setSupportActionBar(mToolbar);
     }
     public void facebookClick(View view){}
     public void twitterClick(View view){}
