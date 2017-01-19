03dc7bc343282e76ef065aa939ca62970ee89344
Cameron Hunter <bg69gl@student.sunderland.ac.uk>
2017-01-19 15:05:04 +0000

    starting the actionbar logo

---
+++ app/src/main/java/com/millennial/sageup/UserDetails.java
@@ -0,0 +1,17 @@
+package com.millennial.sageup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class UserDetails extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details);
+
+        //Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        // setting Toolbar as Action Bar for the App
+       // setSupportActionBar(mToolbar);
+    }
+}
