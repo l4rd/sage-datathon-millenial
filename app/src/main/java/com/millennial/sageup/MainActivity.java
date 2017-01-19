c689cc4b8ca0b91a4ca4e24b86644fd6490354de
Lee Dearden <leedearden16@virginmedia.com>
2017-01-19 15:45:57 +0000

    Added the login fields, sign in button and the information text to the home page.

--- app/src/main/java/com/millennial/sageup/MainActivity.java
+++ app/src/main/java/com/millennial/sageup/MainActivity.java
@@ -1,7 +1,9 @@
 package com.millennial.sageup;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void infoClick(View v) {
+        AlertDialog.Builder adBuilder = new AlertDialog.Builder(MainActivity.this);
+        adBuilder.setTitle("Login Help");
+        adBuilder.setMessage("This will contain the help for the login page.");
+        adBuilder.setPositiveButton("Close", null);
+        AlertDialog dialog = adBuilder.create();
+        dialog.show();
+    }
 }
-// init push test
