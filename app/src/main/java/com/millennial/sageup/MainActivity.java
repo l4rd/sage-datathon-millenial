edb330eb00466057257af85c20219febf7cf5e69
Cameron Hunter <bg69gl@student.sunderland.ac.uk>
2017-01-19 16:21:04 +0000

    redone the onclick

--- app/src/main/java/com/millennial/sageup/MainActivity.java
+++ app/src/main/java/com/millennial/sageup/MainActivity.java
@@ -4,25 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        Button signin = (Button) findViewById(R.id.buttonLogin);
-        signin.setOnClickListener(this);
     }
-    @Override
-    public void onClick(View view) {
-        int id = view.getId();
-        if (id == R.id.buttonLogin) {
-            Intent intent = new Intent(this, UserDetails.class);
-            startActivity(intent);
-        }
+
+
+    public void loginClick(View view) {
+        Intent intent = new Intent(this, UserDetails.class);
+        startActivity(intent);
     }
 }
