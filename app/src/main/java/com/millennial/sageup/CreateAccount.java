ed9eec0abf0c9dff5f21610d89d6612ccfa29116
Lee Dearden <leedearden16@virginmedia.com>
2017-01-19 17:46:20 +0000

    Added the create account Java file.

---
+++ app/src/main/java/com/millennial/sageup/CreateAccount.java
@@ -0,0 +1,19 @@
+package com.millennial.sageup;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by TehLe on 19/01/2017.
+ */
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
+
--- app/src/main/java/com/millennial/sageup/MainActivity.java
+++ app/src/main/java/com/millennial/sageup/MainActivity.java
@@ -1,6 +1,7 @@
 package com.millennial.sageup;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,5 +19,19 @@ public class MainActivity extends AppCompatActivity{
     public void loginClick(View view) {
         Intent intent = new Intent(this, UserDetails.class);
         startActivity(intent);
+    }
+
+    public void infoClick(View v) {
+
+        /*
+        AlertDialog.Builder adBuilder = new AlertDialog.Builder(MainActivity.this);
+        adBuilder.setTitle("Login Help");
+        adBuilder.setMessage("This will contain the help for the login page.");
+        adBuilder.setPositiveButton("Close", null);
+        AlertDialog dialog = adBuilder.create();
+        dialog.show();*/
+
+        Intent accountCreation = new Intent(this, CreateAccount.class);
+        startActivity(accountCreation);
     }
 }
