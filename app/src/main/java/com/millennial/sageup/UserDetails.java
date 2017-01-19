2e75b446d6dfee1bc54652fb85ebb18421fe1a18
Cameron Hunter <bg69gl@student.sunderland.ac.uk>
2017-01-19 19:04:07 +0000

    created the signout functionality

--- app/src/main/java/com/millennial/sageup/UserDetails.java
+++ app/src/main/java/com/millennial/sageup/UserDetails.java
@@ -1,8 +1,13 @@
 package com.millennial.sageup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.MainThread;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class UserDetails extends AppCompatActivity {
@@ -12,9 +17,24 @@ public class UserDetails extends AppCompatActivity {
         setContentView(R.layout.activity_user_details);
 
     }
+
     public void facebookClick(View view){}
     public void twitterClick(View view){}
     public void linkedinClick(View view){}
     public void instagramClick(View view){}
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.signout_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // go back an activity
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+        NavUtils.navigateUpTo(this, intent );
+        return true;
+    }
 }
